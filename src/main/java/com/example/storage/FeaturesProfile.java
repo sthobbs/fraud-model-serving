c23bf5ba89659c79fc6f780e037c012a94828270
Steve Hobbs <hobbs_steve@hotmail.com>
2022-11-15 22:49:32 -0500

    add profile features

---
+++ src/main/java/com/example/storage/FeaturesProfile.java
@@ -0,0 +1,108 @@
+package com.example.storage;
+
+import java.io.Serializable;
+// import java.util.Optional;
+
+import lombok.Data;
+
+
+@Data
+public class FeaturesProfile implements Serializable {
+
+    /*
+    By convention, all profile features begin with "profile", and features that only
+    use the profile (i.e. not current transaction data) are prefixed with "profileRaw". 
+    */
+
+    // Indicator of whether or not we have a profle for this customer
+    int profileRawInd;
+
+    // Amount features
+    double profileRawAmountMin;
+    double profileRawAmountMax;
+    double profileRawAmountAvg;
+    double profileRawAmountStd;
+    double profileRawAmountPercentile10;
+    double profileRawAmountPercentile25;
+    double profileRawAmountPercentile50;
+    double profileRawAmountPercentile75;
+    double profileRawAmountPercentile90;
+    double profileAmountZScore;
+
+    // Time between start of session and first transaction
+    double profileRawMeanSecondsToTransaction;
+    Double profileRawStdSecondsToTransaction;
+    Double profileSecondsToTransactionZScore;
+
+    // Number of sessions with transactions
+    int profileRawSessionCount;
+
+    // Number of transactions
+    int profileRawTransactionCount;
+
+    // Session action count averages
+    double profileRawMeanSessionActionCount;
+    double profileRawMeanSessionAction0Count;
+    double profileRawMeanSessionAction1Count;
+    double profileRawMeanSessionAction2Count;
+    double profileRawMeanSessionAction3Count;
+    double profileRawMeanSessionAction4Count;
+    double profileRawMeanSessionAction5Count;
+    double profileRawMeanSessionAction6Count;
+    double profileRawMeanSessionAction7Count;
+    double profileRawMeanSessionAction8Count;
+    double profileRawMeanSessionAction9Count;
+    
+    // Session action count standard deviations
+    double profileRawStdSessionActionCount;
+    double profileRawStdSessionAction0Count;
+    double profileRawStdSessionAction1Count;
+    double profileRawStdSessionAction2Count;
+    double profileRawStdSessionAction3Count;
+    double profileRawStdSessionAction4Count;
+    double profileRawStdSessionAction5Count;
+    double profileRawStdSessionAction6Count;
+    double profileRawStdSessionAction7Count;
+    double profileRawStdSessionAction8Count;
+    double profileRawStdSessionAction9Count;
+
+    // Session action count z-scores
+    double profileSessionActionCountZScore;
+    double profileSessionAction0CountZScore;
+    double profileSessionAction1CountZScore;
+    double profileSessionAction2CountZScore;
+    double profileSessionAction3CountZScore;
+    double profileSessionAction4CountZScore;
+    double profileSessionAction5CountZScore;
+    double profileSessionAction6CountZScore;
+    double profileSessionAction7CountZScore;
+    double profileSessionAction8CountZScore;
+    double profileSessionAction9CountZScore;
+
+    // Session transaction count averages
+    double profileRawMeanSessionTransactionCount;
+    double profileRawMeanSessionTransactionFromCheckingCount;
+    double profileRawMeanSessionTransactionFromSavingsCount;
+    double profileRawMeanSessionTransactionFromCreditCardCount;
+
+    // Session transaction count standard deviations
+    double profileRawStdSessionTransactionCount;
+    double profileRawStdSessionTransactionFromCheckingCount;
+    double profileRawStdSessionTransactionFromSavingsCount;
+    double profileRawStdSessionTransactionFromCreditCardCount;
+
+    // Session transaction count z-score
+    double profileSessionTransactionCountZScore;
+    double profileSessionTransactionFromCheckingCountZScore;
+    double profileSessionTransactionFromSavingsCountZScore;
+    double profileSessionTransactionFromCreditCardCountZScore;
+
+    // Number of times they previously sent money to this recipient
+    int profileRecipientTxnCount;
+
+    // Number of distinct recipients they previously sent money to
+    long profileDistinctRecipientCount;
+
+
+
+}
---
+++ src/main/java/com/example/transforms/GenFeaturesProfile.java
@@ -0,0 +1,215 @@
+package com.example.transforms;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.example.storage.Action;
+import com.example.storage.FeaturesProfile;
+import com.example.storage.ProfileRecord;
+import com.example.storage.Transaction;
+import java.io.Serializable;
+
+public class GenFeaturesProfile implements Serializable {
+
+    public static FeaturesProfile process(Transaction txn, ProfileRecord profile) {
+
+        FeaturesProfile feats = new FeaturesProfile();
+
+        ArrayList<Action> actions = txn.getActions();
+        
+
+        // Indicator of whether or not we have a profle for this customer
+        feats.setProfileRawInd(profile.getCustomerId() == null ? 0 : 1);
+    
+        // Amount features
+        feats.setProfileRawAmountMin(profile.getAmountMin());
+        feats.setProfileRawAmountMax(profile.getAmountMax());
+        feats.setProfileRawAmountAvg(profile.getAmountAvg());
+        feats.setProfileRawAmountStd(profile.getAmountStd());
+        feats.setProfileRawAmountPercentile10(profile.getAmountPercentile10());
+        feats.setProfileRawAmountPercentile25(profile.getAmountPercentile25());
+        feats.setProfileRawAmountPercentile50(profile.getAmountPercentile50());
+        feats.setProfileRawAmountPercentile75(profile.getAmountPercentile75());
+        feats.setProfileRawAmountPercentile90(profile.getAmountPercentile90());
+        if (profile.getAmountStd() > 0) {
+            feats.setProfileAmountZScore(txn.getAmount() - profile.getAmountAvg() / profile.getAmountStd());
+        }
+        
+        // Time between start of session and first transaction
+        feats.setProfileRawMeanSecondsToTransaction(profile.getMeanSecondsToTransaction());
+        feats.setProfileRawStdSecondsToTransaction(profile.getStdSecondsToTransaction());
+        String dateFormat = "yyyy-MM-dd HH:mm:ss.SSSSSS"; // TODO: put in different file
+        if (profile.getStdSecondsToTransaction() != null) {
+            try {
+                String txnTimeStr = txn.getTimestamp().substring(0, 23);
+                Date txnTime = new SimpleDateFormat(dateFormat).parse(txnTimeStr);
+                String loginTimeStr = actions.get(0).getTimestamp().substring(0, 23);
+                Date loginTime = new SimpleDateFormat(dateFormat).parse(loginTimeStr);
+                Long secondsDiff = (txnTime.getTime() - loginTime.getTime()) / 1000;
+                feats.setProfileSecondsToTransactionZScore(
+                    (secondsDiff - profile.getMeanSecondsToTransaction())
+                    / profile.getStdSecondsToTransaction());
+            }
+            catch (ParseException | ArithmeticException e) {
+                feats.setProfileSecondsToTransactionZScore(null);
+            }
+        }
+        
+        // Number of sessions with transactions
+        feats.setProfileRawSessionCount(profile.getSessionCount());
+
+        // Number of transactions
+        feats.setProfileRawTransactionCount(profile.getTransactionCount());
+
+        // Session action count averages
+        feats.setProfileRawMeanSessionActionCount(profile.getMeanSessionActionCount());
+        feats.setProfileRawMeanSessionAction0Count(profile.getMeanSessionAction0Count());
+        feats.setProfileRawMeanSessionAction1Count(profile.getMeanSessionAction1Count());
+        feats.setProfileRawMeanSessionAction2Count(profile.getMeanSessionAction2Count());
+        feats.setProfileRawMeanSessionAction3Count(profile.getMeanSessionAction3Count());
+        feats.setProfileRawMeanSessionAction4Count(profile.getMeanSessionAction4Count());
+        feats.setProfileRawMeanSessionAction5Count(profile.getMeanSessionAction5Count());
+        feats.setProfileRawMeanSessionAction6Count(profile.getMeanSessionAction6Count());
+        feats.setProfileRawMeanSessionAction7Count(profile.getMeanSessionAction7Count());
+        feats.setProfileRawMeanSessionAction8Count(profile.getMeanSessionAction8Count());
+        feats.setProfileRawMeanSessionAction9Count(profile.getMeanSessionAction9Count());
+
+        // Session action count standard deviations
+        feats.setProfileRawStdSessionActionCount(profile.getStdSessionActionCount());
+        feats.setProfileRawStdSessionAction0Count(profile.getStdSessionAction0Count());
+        feats.setProfileRawStdSessionAction1Count(profile.getStdSessionAction1Count());
+        feats.setProfileRawStdSessionAction2Count(profile.getStdSessionAction2Count());
+        feats.setProfileRawStdSessionAction3Count(profile.getStdSessionAction3Count());
+        feats.setProfileRawStdSessionAction4Count(profile.getStdSessionAction4Count());
+        feats.setProfileRawStdSessionAction5Count(profile.getStdSessionAction5Count());
+        feats.setProfileRawStdSessionAction6Count(profile.getStdSessionAction6Count());
+        feats.setProfileRawStdSessionAction7Count(profile.getStdSessionAction7Count());
+        feats.setProfileRawStdSessionAction8Count(profile.getStdSessionAction8Count());
+        feats.setProfileRawStdSessionAction9Count(profile.getStdSessionAction9Count());
+
+        // Session action count z-scores
+        if (profile.getStdSessionActionCount() > 0) {
+            feats.setProfileSessionActionCountZScore(
+                (actions.size() - profile.getMeanSessionActionCount())
+                / profile.getStdSessionActionCount());
+        }
+        if (profile.getStdSessionAction0Count() > 0) {
+            feats.setProfileSessionAction0CountZScore(
+                (actions.stream().filter(s -> s.getAction().equals("action_0")).count()
+                    - profile.getMeanSessionAction0Count())
+                / profile.getStdSessionAction0Count());
+        }
+        if (profile.getStdSessionAction1Count() > 0) {
+            feats.setProfileSessionAction1CountZScore(
+                (actions.stream().filter(s -> s.getAction().equals("action_1")).count()
+                    - profile.getMeanSessionAction1Count())
+                / profile.getStdSessionAction1Count());
+        }
+        if (profile.getStdSessionAction2Count() > 0) {
+            feats.setProfileSessionAction2CountZScore(
+                (actions.stream().filter(s -> s.getAction().equals("action_2")).count()
+                    - profile.getMeanSessionAction2Count())
+                / profile.getStdSessionAction2Count());
+        }
+        if (profile.getStdSessionAction3Count() > 0) {
+            feats.setProfileSessionAction3CountZScore(
+                (actions.stream().filter(s -> s.getAction().equals("action_3")).count()
+                    - profile.getMeanSessionAction3Count())
+                / profile.getStdSessionAction3Count());
+        }
+        if (profile.getStdSessionAction4Count() > 0) {
+            feats.setProfileSessionAction4CountZScore(
+                (actions.stream().filter(s -> s.getAction().equals("action_4")).count()
+                    - profile.getMeanSessionAction4Count())
+                / profile.getStdSessionAction4Count());
+        }
+        if (profile.getStdSessionAction5Count() > 0) {
+            feats.setProfileSessionAction5CountZScore(
+                (actions.stream().filter(s -> s.getAction().equals("action_5")).count()
+                    - profile.getMeanSessionAction5Count())
+                / profile.getStdSessionAction5Count());
+        }
+        if (profile.getStdSessionAction6Count() > 0) {
+            feats.setProfileSessionAction6CountZScore(
+                (actions.stream().filter(s -> s.getAction().equals("action_6")).count()
+                    - profile.getMeanSessionAction6Count())
+                / profile.getStdSessionAction6Count());
+        }
+        if (profile.getStdSessionAction7Count() > 0) {
+            feats.setProfileSessionAction7CountZScore(
+                (actions.stream().filter(s -> s.getAction().equals("action_7")).count()
+                    - profile.getMeanSessionAction7Count())
+                / profile.getStdSessionAction7Count());
+        }
+        if (profile.getStdSessionAction8Count() > 0) {
+            feats.setProfileSessionAction8CountZScore(
+                (actions.stream().filter(s -> s.getAction().equals("action_8")).count()
+                    - profile.getMeanSessionAction8Count())
+                / profile.getStdSessionAction8Count());
+        }
+        if (profile.getStdSessionAction9Count() > 0) {
+            feats.setProfileSessionAction9CountZScore(
+                (actions.stream().filter(s -> s.getAction().equals("action_9")).count()
+                    - profile.getMeanSessionAction9Count())
+                / profile.getStdSessionAction9Count());
+        }
+
+        // Session transaction count averages
+        feats.setProfileRawMeanSessionTransactionCount(profile.getMeanSessionTransactionCount());
+        feats.setProfileRawMeanSessionTransactionFromCheckingCount(profile.getMeanSessionTransactionFromCheckingCount());
+        feats.setProfileRawMeanSessionTransactionFromSavingsCount(profile.getMeanSessionTransactionFromSavingsCount());
+        feats.setProfileRawMeanSessionTransactionFromCreditCardCount(profile.getMeanSessionTransactionFromCreditCardCount());
+
+        // Session transaction count standard deviations
+        feats.setProfileRawStdSessionTransactionCount(profile.getStdSessionTransactionCount());
+        feats.setProfileRawStdSessionTransactionFromCheckingCount(profile.getStdSessionTransactionFromCheckingCount());
+        feats.setProfileRawStdSessionTransactionFromSavingsCount(profile.getStdSessionTransactionFromSavingsCount());
+        feats.setProfileRawStdSessionTransactionFromCreditCardCount(profile.getStdSessionTransactionFromCreditCardCount());
+
+        // Session transaction count z-score
+        if (profile.getStdSessionTransactionCount() > 0) {
+            feats.setProfileSessionTransactionCountZScore(
+                (actions.stream().filter(s -> s.getAction().equals("transaction")).count()
+                    - profile.getMeanSessionTransactionCount())
+                / profile.getStdSessionTransactionCount());
+        }
+        if (profile.getStdSessionTransactionFromCheckingCount() > 0) {
+            feats.setProfileSessionTransactionFromCheckingCountZScore(
+                (actions.stream().filter(s -> s.getAction().equals("transaction")
+                        && s.getAccountType().equals("checking")).count()
+                    - profile.getMeanSessionTransactionFromCheckingCount())
+                / profile.getStdSessionTransactionFromCheckingCount());
+        }
+        if (profile.getStdSessionTransactionFromSavingsCount() > 0) {
+            feats.setProfileSessionTransactionFromSavingsCountZScore(
+                (actions.stream().filter(s -> s.getAction().equals("transaction")
+                        && s.getAccountType().equals("savings")).count()
+                    - profile.getMeanSessionTransactionFromSavingsCount())
+                / profile.getStdSessionTransactionFromSavingsCount());
+        }
+        if (profile.getStdSessionTransactionFromCreditCardCount() > 0) {
+            feats.setProfileSessionTransactionFromCreditCardCountZScore(
+                (actions.stream().filter(s -> s.getAction().equals("transaction")
+                        && s.getAccountType().equals("credit_card")).count()
+                    - profile.getMeanSessionTransactionFromCreditCardCount())
+                / profile.getStdSessionTransactionFromCreditCardCount());
+        }
+
+        // Number of times they previously sent money to this recipient
+        String recipient = txn.getRecipient();
+        feats.setProfileRecipientTxnCount(profile.getRecipients().stream()
+            .filter(s -> s.getRecipient().equals(recipient))
+            .map(s -> s.getTxnCnt())
+            .findFirst().orElse(0)); //.get());
+       
+        // Number of distinct recipients they previously sent money to
+        feats.setProfileDistinctRecipientCount(profile.getRecipients().stream()
+            .map(s -> s.getRecipient())
+            .distinct()
+            .count());
+
+        return feats;
+    }
+}
