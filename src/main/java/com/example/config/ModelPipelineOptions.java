2a7945a7a0512eb99a5c85cad9c881ea30a60ded
Steve Hobbs <hobbs_steve@hotmail.com>
2022-11-18 22:08:53 -0500

    add dataflow pipeline options

--- src/main/java/com/example/config/ModelPipelineOptions.java
+++ src/main/java/com/example/config/ModelPipelineOptions.java
@@ -1,12 +1,13 @@
 package com.example.config;
 
-import org.apache.beam.sdk.options.PipelineOptions;
+// import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.Description;
+import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.options.Default;
 
 
 
-public interface ModelPipelineOptions extends PipelineOptions {
+public interface ModelPipelineOptions extends DataflowPipelineOptions {
 
     @Description("input path to disk or pubsub subscription")
     String getInputPath();
@@ -17,6 +18,18 @@ public interface ModelPipelineOptions extends PipelineOptions {
     String getInputType();
     void setInputType(String input);
 
+    @Description("profile side input path")
+    String getProfileSideInputPath();
+    void setProfileSideInputPath(String input);
+
+    @Description("customer info side input path")
+    String getCustomerInfoSideInputPath();
+    void setCustomerInfoSideInputPath(String input);
+
+    @Description("model path")
+    String getModelPath();
+    void setModelPath(String input);
+
     @Description("prefix of output path")
     String getOutputPath();
     void setOutputPath(String output);
@@ -24,4 +37,9 @@ public interface ModelPipelineOptions extends PipelineOptions {
     @Description("type of input destination (e.g. 'disk', 'pubsub')")
     String getOutputType();
     void setOutputType(String output);
+
+    @Description("project id")
+    @Default.String("analog-arbor-367702")
+    String getProjectId();
+    void setProjectId(String input);
 }
